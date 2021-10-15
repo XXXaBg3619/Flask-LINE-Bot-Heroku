0c0574a2bd9a3d3222875dff4c7e0ef49d66ab96
XXXaBg3619 <71827144+XXXaBg3619@users.noreply.github.com>
2021-10-15 23:11:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,9 +13,9 @@ import random
 
 app = Flask(__name__)
 
-
 line_bot_api = LineBotApi("S1NRUscHr3pXdpnYh28UZlZmeEnmEbfX6rkSC3WHo/zSbBxUJcKgLEGtOoTlaHB7ntc/QBgAKFcwDuEvM5Kmtwhph1DdYBOeCcVB+N7Cnt9KRyrjdR6vA/+KONhX/VBvK+fqUq6yFpxsahuV3YRPQAdB04t89/1O/w1cDnyilFU=")
 handler = WebhookHandler("e104139d44baead65940861cbf50b707")
+
 
 # 接收 LINE 的資訊
 @app.route("/callback", methods=['POST'])
@@ -32,34 +32,44 @@ def callback():
 
     return 'OK'
 
-# 請 google 幫我們找圖
-base = "https://www.google.com/search?"
+# 請 pixabay 幫我們找圖
 @handler.add(MessageEvent, message=TextMessage)
 def pixabay_isch(event):
-    try:
-        url = base + urllib.parse.urlencode({'q':event.message.text})[2:]
-        headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'}
-        req = urllib.request.Request(url, headers = headers)
-        conn = urllib.request.urlopen(req)
-        
-        pattern = 'img srcset="\S*\s\w*,'
-        img_list = []
-        for match in re.finditer(pattern, str(conn.read())):
-            img_list.append(match.group()[12:-3])
-        random_img_url = img_list[random.randint(0, len(img_list)+1)]
+    
+    if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef":
+        try:
+            url = f"https://pixabay.com/images/search/{urllib.parse.urlencode({'q':event.message.text})[2:]}/"
+            headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'}
             
-        line_bot_api.reply_message(
-            event.reply_token,
-            ImageSendMessage(
-                original_content_url=random_img_url,
-                preview_image_url=random_img_url
+            req = urllib.request.Request(url, headers = headers)
+            conn = urllib.request.urlopen(req)
+            
+            print('fetch page finish')
+            
+            pattern = 'img srcset="\S*\s\w*,'
+            img_list = []
+            
+            for match in re.finditer(pattern, str(conn.read())):
+                img_list.append(match.group()[12:-3])
+                
+            random_img_url = img_list[random.randint(0, len(img_list)+1)]
+            print('fetch img url finish')
+            print(random_img_url)
+            
+            line_bot_api.reply_message(
+                event.reply_token,
+                ImageSendMessage(
+                    original_content_url=random_img_url,
+                    preview_image_url=random_img_url
+                )
             )
-        )
-    # 如果找不到圖，就學你說話
-    except:
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=event.message.text)
-        )
+        # 如果找不到圖，就學你說話
+        except:
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=event.message.text)
+            )
+            pass
+
 if __name__ == "__main__":
     app.run()
