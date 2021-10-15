709ad47ef56e309fcc712704701aca093df98004
XXXaBg3619 <71827144+XXXaBg3619@users.noreply.github.com>
2021-10-16 01:50:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -35,43 +35,32 @@ def callback():
 # 請 pixabay 幫我們找圖
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    
-    if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef":
-        try:
-                        
-            img_list = []
+    try:
+        img_list = []
+        img_search = {'tbm': 'isch', 'q': event.message.text}
+        query = urllib.parse.urlencode(img_search)
+        base  = "https://www.google.com/search?"
+        url   = str(base+query)
+        headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'}
+        res  = urllib.request.Request(url, headers=headers)
+        con  = urllib.request.urlopen(res)
+        data = con.read()
+        pattern = '"(https://encrypted-tbn0.gstatic.com[\S]*)"'
+        for match in re.finditer(pattern, str(data, "utf-8")):
+            img_list.append(match.group(1))
+        random_img_url = img_list[random.randint(0, len(img_list)+1)]
 
-            img_search = {'tbm': 'isch', 'q': event.message.text}
-            query = urllib.parse.urlencode(img_search)
-            base  = "https://www.google.com/search?"
-            url   = str(base+query)
-
-            headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'}
-
-            res  = urllib.request.Request(url, headers=headers)
-            con  = urllib.request.urlopen(res)
-            data = con.read()
-
-            pattern = '"(https://encrypted-tbn0.gstatic.com[\S]*)"'
-
-            for match in re.finditer(pattern, str(data, "utf-8")):
-                if len(match.group(1)) < 150:
-                    img_list.append(match.group(1))
-
-            random_img_url = img_list[random.randint(0, len(img_list)+1)]
-
-            message = ImageSendMessage(
-                original_content_url = random_img_url,
-                preview_image_url    = random_img_url
-            )
-            line_bot_api.reply_message(event.reply_token, message)
-        # 如果找不到圖，就學你說話
-        except:
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text=event.message.text)
-            )
-            pass
+        message = ImageSendMessage(
+            original_content_url = random_img_url,
+            preview_image_url    = random_img_url
+        )
+        line_bot_api.reply_message(event.reply_token, message)
+    # 如果找不到圖，就學你說話
+    except:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event.message.text)
+        )
 
 if __name__ == "__main__":
     app.run()
